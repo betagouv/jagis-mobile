f4e2541ae6d23a7d0443878fcb392ca38ecb9144
Emile Rolley ⏚ <44124798+EmileRolley@users.noreply.github.com>
2025-03-10 15:55:41 +0100

    refactor(questions): use a generic cusor manager from list (#561)

--- app/lib/features/actions_recommanded/infrastructure/actions_recommanded_manager.dart
+++ app/lib/features/actions_recommanded/infrastructure/actions_recommanded_manager.dart
@@ -2,52 +2,16 @@ import 'package:app/core/infrastructure/dio_http_client.dart';
 import 'package:app/core/infrastructure/endpoints.dart';
 import 'package:app/features/know_your_customer/core/domain/question.dart';
 import 'package:app/features/know_your_customer/core/infrastructure/question_mapper.dart';
-import 'package:app/features/questions_manager/domain/cursor.dart';
-import 'package:app/features/questions_manager/domain/cursor_manager.dart';
+import 'package:app/features/questions_manager/domain/cursor_manager_from_list.dart';
 
-// TODO(erolley): This could be abstracted to be used for all list based manager.
-class ActionsRecommandedQuestionsManager extends CursorManager<Question> {
-  const ActionsRecommandedQuestionsManager({required final DioHttpClient client, required final String sequenceId})
-    : _client = client,
-      _sequenceId = sequenceId;
+class ActionsRecommandedQuestionsManager extends CursorManagerFromList<Question> {
+  ActionsRecommandedQuestionsManager({required final DioHttpClient client, required final String sequenceId})
+    : super(
+        getList: () async {
+          final response = await client.get(Endpoints.questions(sequenceId));
+          final data = response.data as List<dynamic>;
 
-  final DioHttpClient _client;
-  final String _sequenceId;
-
-  @override
-  Future<Cursor<Question>> first() async {
-    final list = await _getList();
-
-    return list.isEmpty
-        ? const Cursor(element: null, index: 0, total: 0)
-        : Cursor(element: list.first, index: 0, total: list.length);
-  }
-
-  @override
-  Future<Cursor<Question>> previous(final Cursor<Question> current) async {
-    final list = await _getList();
-    final previousIndex = current.index - 1;
-
-    return previousIndex < 0
-        ? Cursor(element: list.first, index: 0, total: list.length)
-        : Cursor(element: list[previousIndex], index: previousIndex, total: list.length);
-  }
-
-  @override
-  Future<Cursor<Question>> next(final Cursor<Question> current) async {
-    final list = await _getList();
-    final nextIndex = current.index + 1;
-
-    return nextIndex >= list.length
-        ? Cursor(element: null, index: current.index, total: list.length)
-        : Cursor(element: list[nextIndex], index: nextIndex, total: list.length);
-  }
-
-  Future<List<Question>> _getList() async {
-    final url = Endpoints.questions(_sequenceId);
-    final response = await _client.get(url);
-    final data = response.data as List<dynamic>;
-
-    return data.cast<Map<String, dynamic>>().map(QuestionMapper.fromJson).whereType<Question>().toList();
-  }
+          return data.cast<Map<String, dynamic>>().map(QuestionMapper.fromJson).whereType<Question>().toList();
+        },
+      );
 }
--- app/lib/features/car_simulator/infrastructure/car_simulator_questions_manager.dart
+++ app/lib/features/car_simulator/infrastructure/car_simulator_questions_manager.dart
@@ -4,58 +4,26 @@ import 'package:app/features/action/domain/action.dart';
 import 'package:app/features/actions/domain/action_type.dart';
 import 'package:app/features/know_your_customer/core/domain/question.dart';
 import 'package:app/features/know_your_customer/core/infrastructure/question_mapper.dart';
-import 'package:app/features/questions_manager/domain/cursor.dart';
-import 'package:app/features/questions_manager/domain/cursor_manager.dart';
+import 'package:app/features/questions_manager/domain/cursor_manager_from_list.dart';
 
-// TODO(erolley): This could be abstracted to be used for all list based manager.
-class CarSimulatorQuestionsManager extends CursorManager<Question> {
-  const CarSimulatorQuestionsManager({required final DioHttpClient client}) : _client = client;
+class CarSimulatorQuestionsManager extends CursorManagerFromList<Question> {
+  CarSimulatorQuestionsManager({required final DioHttpClient client})
+    : super(
+        getList: () async {
+          final response = await client.get(
+            Endpoints.action(
+              type: actionTypeToAPIString(ActionType.simulator),
+              code: actionSimulatorIdToAPIString(ActionSimulatorId.carSimulator),
+            ),
+          );
 
-  final DioHttpClient _client;
+          final data = response.data as Map<String, dynamic>;
 
-  @override
-  Future<Cursor<Question>> first() async {
-    final list = await _getList();
-
-    return list.isEmpty
-        ? const Cursor(element: null, index: 0, total: 0)
-        : Cursor(element: list.first, index: 0, total: list.length);
-  }
-
-  @override
-  Future<Cursor<Question>> previous(final Cursor<Question> current) async {
-    final list = await _getList();
-    final previousIndex = current.index - 1;
-
-    return previousIndex < 0
-        ? Cursor(element: list.first, index: 0, total: list.length)
-        : Cursor(element: list[previousIndex], index: previousIndex, total: list.length);
-  }
-
-  @override
-  Future<Cursor<Question>> next(final Cursor<Question> current) async {
-    final list = await _getList();
-    final nextIndex = current.index + 1;
-
-    return nextIndex >= list.length
-        ? Cursor(element: null, index: current.index, total: list.length)
-        : Cursor(element: list[nextIndex], index: nextIndex, total: list.length);
-  }
-
-  Future<List<Question>> _getList() async {
-    final response = await _client.get(
-      Endpoints.action(
-        type: actionTypeToAPIString(ActionType.simulator),
-        code: actionSimulatorIdToAPIString(ActionSimulatorId.carSimulator),
-      ),
-    );
-
-    final data = response.data as Map<String, dynamic>;
-
-    return (data['kycs'] as List<dynamic>)
-        .cast<Map<String, dynamic>>()
-        .map(QuestionMapper.fromJson)
-        .whereType<Question>()
-        .toList();
-  }
+          return (data['kycs'] as List<dynamic>)
+              .cast<Map<String, dynamic>>()
+              .map(QuestionMapper.fromJson)
+              .whereType<Question>()
+              .toList();
+        },
+      );
 }

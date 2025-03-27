1e3ea9e0c62758880b5b99a9608a018c8cd601a1
Emile Rolley <emile.rolley@tuta.io>
2025-03-27 12:14:44 +0100

    feat(home): ajoute la possiblité de refresh la home avec une animation des compteurs

--- app/lib/features/home/presentation/widgets/home_content_layout.dart
+++ app/lib/features/home/presentation/widgets/home_content_layout.dart
@@ -2,9 +2,11 @@ import 'package:app/core/presentation/widgets/fondamentaux/rounded_rectangle_bor
 import 'package:app/features/aids/core/presentation/widgets/aids_section.dart';
 import 'package:app/features/environmental_performance/home/presentation/widgets/environmental_performance_section.dart';
 import 'package:app/features/home/home_dashboard/widget/home_dashboard.dart';
+import 'package:app/features/home/presentation/pages/home_page.dart';
 import 'package:app/features/survey/survey_section.dart';
 import 'package:dsfr/dsfr.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomeContentLayout extends StatelessWidget {
   const HomeContentLayout({super.key});
@@ -13,18 +15,26 @@ class HomeContentLayout extends StatelessWidget {
   Widget build(final context) {
     const spacing = SizedBox(height: DsfrSpacings.s4w);
 
-    return ListView(
-      padding: EdgeInsets.zero,
-      children: const [
-        HomeDashboard(),
-        // ThemeHubSection(),
-        spacing,
-        Padding(padding: EdgeInsets.symmetric(horizontal: paddingVerticalPage), child: EnvironmentalPerformanceSection()),
-        spacing,
-        Padding(padding: EdgeInsets.symmetric(horizontal: paddingVerticalPage), child: AidsSection()),
-        spacing,
-        SurveySection(),
-      ],
+    return RefreshIndicator(
+      onRefresh: () async {
+        if (GoRouter.of(context).canPop()) {
+          GoRouter.of(context).pop();
+        }
+        await GoRouter.of(context).pushNamed(HomePage.name);
+      },
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: const [
+          HomeDashboard(),
+          // ThemeHubSection(),
+          spacing,
+          Padding(padding: EdgeInsets.symmetric(horizontal: paddingVerticalPage), child: EnvironmentalPerformanceSection()),
+          spacing,
+          Padding(padding: EdgeInsets.symmetric(horizontal: paddingVerticalPage), child: AidsSection()),
+          spacing,
+          SurveySection(),
+        ],
+      ),
     );
   }
 }

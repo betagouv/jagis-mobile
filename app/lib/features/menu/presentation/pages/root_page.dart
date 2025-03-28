878fa8cc207ebda9feecc86cc36046b64ce0e33b
Lucas SAUDON <lucas.saudon@beta.gouv.fr>
2025-03-28 11:27:50 +0100

    fix: si l'on ouvre la page aides via une action on peut revenir sur l'action

--- app/lib/features/menu/presentation/pages/root_page.dart
+++ app/lib/features/menu/presentation/pages/root_page.dart
@@ -2,6 +2,7 @@ import 'package:app/core/presentation/widgets/composants/app_bar.dart';
 import 'package:app/core/presentation/widgets/composants/scaffold.dart';
 import 'package:app/features/menu/presentation/widgets/menu.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class RootPage extends StatelessWidget {
   const RootPage({super.key, this.title, this.appBarBottom, required this.body});
@@ -10,7 +11,21 @@ class RootPage extends StatelessWidget {
   final PreferredSizeWidget? appBarBottom;
   final Widget body;
 
+  /// Retourne true si la page est à la racine de l'application
+  ///
+  /// go_router utilise garde la route '/' dans la configuration de la route (peut-être une erreur de ma part)
+  /// ```text
+  /// '/' => true
+  /// '/', '/aides' => true
+  /// '/', '/', '/aides => false
+  /// '/', '/actions', '/actions/1', /aides => false
+  /// ```
+  bool _isAtRootLevel(final BuildContext context) => GoRouter.of(context).routerDelegate.currentConfiguration.matches.length <= 2;
+
   @override
-  Widget build(final context) =>
-      FnvScaffold(appBar: FnvAppBar(title: title, bottom: appBarBottom, isRoot: true), body: body, drawer: const Menu());
+  Widget build(final context) => FnvScaffold(
+    appBar: FnvAppBar(title: title, bottom: appBarBottom, isRoot: _isAtRootLevel(context)),
+    body: body,
+    drawer: const Menu(),
+  );
 }

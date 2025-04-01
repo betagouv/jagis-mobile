7adfc8c48a7226eb0a8d3757abf518ee7878c975
Lucas SAUDON <lucas.saudon@beta.gouv.fr>
2025-04-01 17:04:48 +0200

    feat(feedback): supprimer la section d'avis

--- app/lib/features/survey/survey_l10n.dart
+++
@@ -1,7 +0,0 @@
-abstract final class SurveySectionL10n {
-  const SurveySectionL10n._();
-
-  static const title = "J'agis est un nouveau service qui continue de s’améliorer";
-  static const button = 'Donner mon avis';
-  static const link = 'https://thread-origami-ae6.notion.site/130576eceac1804b8863d874f7f1fd1e';
-}

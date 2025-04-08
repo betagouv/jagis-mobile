c83d323ec0535f4f5edbca88ce3d9318b45b87f4
Emile Rolley <emile.rolley@tuta.io>
2025-04-08 15:34:06 +0200

    refactor(questions): utilise un seul widget pour parcourir un enchainement de questions

--- app/lib/features/car_simulator/presentation/widgets/car_simulator_widget.dart
+++ app/lib/features/car_simulator/presentation/widgets/car_simulator_widget.dart
@@ -5,16 +5,10 @@ import 'package:app/features/actions/domain/action_type.dart';
 import 'package:app/features/car_simulator/presentation/car_simulator_result/bloc/car_simulator_result_bloc.dart';
 import 'package:app/features/car_simulator/presentation/car_simulator_result/bloc/car_simulator_result_event.dart';
 import 'package:app/features/car_simulator/presentation/car_simulator_result/widgets/car_simulator_result.dart';
-import 'package:app/features/know_your_customer/core/domain/question.dart';
-import 'package:app/features/know_your_customer/core/domain/question_code.dart';
-import 'package:app/features/know_your_customer/detail/presentation/form/input_controller.dart';
-import 'package:app/features/know_your_customer/detail/presentation/form/question_controller.dart';
-import 'package:app/features/know_your_customer/detail/presentation/form/question_form.dart';
 import 'package:app/features/questions_manager/bloc/questions_manager_bloc.dart';
 import 'package:app/features/questions_manager/bloc/questions_manager_event.dart';
 import 'package:app/features/questions_manager/bloc/questions_manager_state.dart';
-import 'package:app/features/questions_manager/domain/cursor.dart';
-import 'package:app/features/questions_manager/presentation/questions_manager_buttons_widget.dart';
+import 'package:app/features/questions_manager/presentation/questions_manager_question_view.dart';
 import 'package:app/l10n/l10n.dart';
 import 'package:dsfr/dsfr.dart';
 import 'package:flutter/material.dart';
@@ -94,47 +88,9 @@ class _Success extends StatelessWidget {
               ),
             ),
           QuestionStepper(current: cursor.index + 1, total: cursor.total),
-          _QuestionWidget(key: ValueKey(cursor.element), code: cursor.element!.code, cursor: cursor),
+          QuestionsManagerQuestionView(key: ValueKey(cursor.element), cursor: cursor),
         ],
       ),
     );
   }
-}
-
-class _QuestionWidget extends StatefulWidget {
-  const _QuestionWidget({super.key, required this.code, required this.cursor});
-
-  final QuestionCode code;
-  final Cursor<Question> cursor;
-
-  @override
-  State<_QuestionWidget> createState() => _QuestionWidgetState();
-}
-
-class _QuestionWidgetState extends State<_QuestionWidget> {
-  final _questionController = QuestionController();
-  final _inputController = InputController();
-
-  @override
-  void dispose() {
-    _questionController.dispose();
-    _inputController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(final context) => Column(
-    spacing: DsfrSpacings.s3w,
-    children: [
-      QuestionForm(
-        questionId: widget.code.value,
-        questionController: _questionController,
-        inputController: _inputController,
-        onSaved: () {
-          context.read<QuestionsManagerBloc>().add(const QuestionsManagerNextRequested());
-        },
-      ),
-      QuestionsManagerButtons(cursor: widget.cursor, questionController: _questionController, inputController: _inputController),
-    ],
-  );
 }
---
+++ app/lib/features/questions_manager/presentation/questions_manager_question_view.dart
@@ -0,0 +1,48 @@
+import 'package:app/features/know_your_customer/core/domain/question.dart';
+import 'package:app/features/know_your_customer/detail/presentation/form/input_controller.dart';
+import 'package:app/features/know_your_customer/detail/presentation/form/question_controller.dart';
+import 'package:app/features/know_your_customer/detail/presentation/form/question_form.dart';
+import 'package:app/features/questions_manager/bloc/questions_manager_bloc.dart';
+import 'package:app/features/questions_manager/bloc/questions_manager_event.dart';
+import 'package:app/features/questions_manager/domain/cursor.dart';
+import 'package:app/features/questions_manager/presentation/questions_manager_buttons_widget.dart';
+import 'package:dsfr/dsfr.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class QuestionsManagerQuestionView extends StatefulWidget {
+  const QuestionsManagerQuestionView({super.key, required this.cursor});
+
+  final Cursor<Question> cursor;
+
+  @override
+  State<QuestionsManagerQuestionView> createState() => _QuestionWidgetState();
+}
+
+class _QuestionWidgetState extends State<QuestionsManagerQuestionView> {
+  final _questionController = QuestionController();
+  final _inputController = InputController();
+
+  @override
+  void dispose() {
+    _questionController.dispose();
+    _inputController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(final context) => Column(
+    spacing: DsfrSpacings.s3w,
+    children: [
+      QuestionForm(
+        questionId: widget.cursor.element!.code.value,
+        questionController: _questionController,
+        inputController: _inputController,
+        onSaved: () {
+          context.read<QuestionsManagerBloc>().add(const QuestionsManagerNextRequested());
+        },
+      ),
+      QuestionsManagerButtons(cursor: widget.cursor, questionController: _questionController, inputController: _inputController),
+    ],
+  );
+}

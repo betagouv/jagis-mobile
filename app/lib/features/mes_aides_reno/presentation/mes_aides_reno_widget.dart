1a96dd42b5dec6851e74d09fd318ebe3695a7f11
Emile Rolley <emile.rolley@tuta.io>
2025-04-15 12:42:46 +0200

    fix(mes_aides_reno): gestion de la validation de fin d'action

--- app/lib/features/mes_aides_reno/presentation/mes_aides_reno_widget.dart
+++ app/lib/features/mes_aides_reno/presentation/mes_aides_reno_widget.dart
@@ -24,22 +24,13 @@ class _View extends StatelessWidget {
   const _View();
 
   @override
-  Widget build(final BuildContext context) => BlocConsumer<MesAidesRenoBloc, MesAidesRenoState>(
+  Widget build(final BuildContext context) => BlocBuilder<MesAidesRenoBloc, MesAidesRenoState>(
     builder:
         (final context, final state) => switch (state.status) {
-          MesAidesRenoStateStatus.initial => const SizedBox.shrink(),
-          MesAidesRenoStateStatus.loading => const Center(child: CircularProgressIndicator()),
-          MesAidesRenoStateStatus.getIframeUrlSuccess => _Success(state.iframeUrl!),
-          MesAidesRenoStateStatus.sendSituationSuccess => const Center(child: Text('Votre situation a bien été envoyée')),
+          MesAidesRenoStateStatus.initial || MesAidesRenoStateStatus.loading => const Center(child: CircularProgressIndicator()),
+          MesAidesRenoStateStatus.success => _Success(state.iframeUrl!),
           MesAidesRenoStateStatus.failure => Center(child: Text(state.errorMessage ?? 'Erreur inconnue')),
         },
-    listener: (final context, final state) {
-      if (state.status == MesAidesRenoStateStatus.sendSituationSuccess) {
-        context.read<ActionBloc>().add(
-          ActionMarkAsDone(id: ActionSimulatorId.mesAidesReno.apiString, type: ActionType.simulator),
-        );
-      }
-    },
   );
 }
 
@@ -67,7 +58,6 @@ class _MesAidesRenoWidgetState extends State<_Success> {
   @override
   Widget build(final BuildContext context) {
     final url = WebUri(widget.iframeUrl).replace(host: 'reno-git-fork-emilerolley-master-mesaidesreno.vercel.app');
-    print('MesAidesRenoWidget: ${url.host}');
     return SizedBox(
       height: _webViewHeight,
       child: InAppWebView(

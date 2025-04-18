f67ed49b2875e5b9f766eef1961cc20b3ff4ddc6
Lucas SAUDON <lucas.saudon@beta.gouv.fr>
2025-04-18 13:10:42 +0200

    refactor(mappage): améliorer la construction de l'adresse dans fromJson
    La méthode fromJson a été modifiée pour construire l'adresse à partir
    des composants individuels, en s'assurant que les parties nulles ou
    vides ne sont pas incluses.

--- app/lib/features/services/pres_de_chez_vous/infrastructure/pres_de_chez_vous_summary_mapper.dart
+++ app/lib/features/services/pres_de_chez_vous/infrastructure/pres_de_chez_vous_summary_mapper.dart
@@ -3,9 +3,17 @@ import 'package:app/features/services/pres_de_chez_vous/domain/pres_de_chez_vous
 abstract final class PresDeChezVousSummaryMapper {
   const PresDeChezVousSummaryMapper._();
 
-  static PresDeChezVousSummary fromJson(final Map<String, dynamic> json) => PresDeChezVousSummary(
-    name: json['titre'] as String,
-    address: '${json['adresse_rue'] as String} ${json['adresse_code_postal'] as String} ${json['adresse_nom_ville'] as String}',
-    distanceInMeters: json['distance_metres'] as int,
-  );
+  static PresDeChezVousSummary fromJson(final Map<String, dynamic> json) {
+    final address = [
+      json['adresse_rue'] as String?,
+      json['adresse_code_postal'] as String?,
+      json['adresse_nom_ville'] as String?,
+    ].where((final part) => part != null && part.isNotEmpty).join(' ');
+
+    return PresDeChezVousSummary(
+      name: json['titre'] as String,
+      address: address,
+      distanceInMeters: json['distance_metres'] as int,
+    );
+  }
 }

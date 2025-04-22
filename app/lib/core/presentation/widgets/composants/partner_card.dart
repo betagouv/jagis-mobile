0729d846f4a469ab79ff528d268889704363800d
Lucas SAUDON <lucas.saudon@beta.gouv.fr>
2025-04-22 15:46:44 +0200

    style(partenaire): mise à jour du design

--- app/lib/core/presentation/widgets/composants/partner_card.dart
+++ app/lib/core/presentation/widgets/composants/partner_card.dart
@@ -1,5 +1,7 @@
 import 'package:app/core/infrastructure/url_launcher.dart';
 import 'package:app/core/presentation/widgets/composants/image.dart';
+import 'package:app/core/presentation/widgets/fondamentaux/colors.dart';
+import 'package:app/core/presentation/widgets/fondamentaux/shadows.dart';
 import 'package:app/features/environmental_performance/summary/environmental_performance_summary_l10n.dart';
 import 'package:dsfr/dsfr.dart';
 import 'package:flutter/material.dart';
@@ -21,48 +23,37 @@ class PartnerCard extends StatelessWidget {
   final String logo;
 
   @override
-  Widget build(final BuildContext context) {
-    const borderColor = Color(0xffb1b1ff);
-
-    return DecoratedBox(
-      decoration: const BoxDecoration(color: Color(0xffeef2ff), border: Border.fromBorderSide(BorderSide(color: borderColor))),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 1, top: 1, right: 1),
-            child: ColoredBox(
-              color: Colors.white,
-              child: FnvImage.asset(image, alignment: Alignment.topCenter, height: 170, fit: BoxFit.cover),
-            ),
+  Widget build(final BuildContext context) => DecoratedBox(
+    decoration: const BoxDecoration(color: FnvColors.carteFond, boxShadow: cardBoxShadow),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        FnvImage.asset(image, alignment: Alignment.topCenter, height: 137, fit: BoxFit.cover),
+        Padding(
+          padding: const EdgeInsets.all(DsfrSpacings.s3w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                EnvironmentalPerformanceSummaryL10n.proposePar,
+                style: DsfrTextStyle.bodySmItalic(color: DsfrColors.blueFranceSun113),
+              ),
+              Text(name, style: const DsfrTextStyle.headline5()),
+              const SizedBox(height: DsfrSpacings.s2w),
+              Text(description, style: const DsfrTextStyle.bodySm()),
+              const SizedBox(height: DsfrSpacings.s2w),
+              DsfrLink.md(
+                label: url,
+                onTap: () async {
+                  await FnvUrlLauncher.launch(url);
+                },
+              ),
+              const SizedBox(height: DsfrSpacings.s3w),
+              FnvImage.asset(logo, height: 40, semanticLabel: 'Logo de $name'),
+            ],
           ),
-          const DsfrDivider(color: borderColor),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: DsfrSpacings.s1w, horizontal: DsfrSpacings.s3w),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  EnvironmentalPerformanceSummaryL10n.proposePar,
-                  style: DsfrTextStyle.bodySmItalic(color: DsfrColors.blueFranceSun113),
-                ),
-                Text(name, style: const DsfrTextStyle.headline5()),
-                const SizedBox(height: DsfrSpacings.s1w),
-                Text(description, style: const DsfrTextStyle(fontSize: 15)),
-                const SizedBox(height: DsfrSpacings.s1w),
-                DsfrLink.md(
-                  label: url,
-                  onTap: () async {
-                    await FnvUrlLauncher.launch(url);
-                  },
-                ),
-                const SizedBox(height: DsfrSpacings.s2w),
-                FnvImage.asset(logo, height: 40, semanticLabel: 'Logo de $name'),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+        ),
+      ],
+    ),
+  );
 }

75a6aaeceef806fb7ae186767b690059f7748a10
Lucas SAUDON <lucas.saudon@beta.gouv.fr>
2025-05-12 20:39:35 +0200

    fix(action): prendre en compte le markdown dans les titres

--- app/lib/features/action/presentation/widgets/action_markdown_heading.dart
+++ app/lib/features/action/presentation/widgets/action_markdown_heading.dart
@@ -2,13 +2,14 @@ import 'package:app/core/infrastructure/markdown.dart';
 import 'package:dsfr/dsfr.dart';
 import 'package:flutter/material.dart';
 import 'package:gpt_markdown/custom_widgets/markdown_config.dart';
+import 'package:gpt_markdown/gpt_markdown.dart';
 
 @immutable
 class ActionMarkdownHeading extends FNVHTag {
-  ActionMarkdownHeading({required this.icon, required this.color});
+  ActionMarkdownHeading({required this.icon, required this.iconColor});
 
   final IconData icon;
-  final Color color;
+  final Color iconColor;
 
   @override
   Widget build(final BuildContext context, final String text, final GptMarkdownConfig config) {
@@ -23,13 +24,24 @@ class ActionMarkdownHeading extends FNVHTag {
       return super.build(context, text, config);
     }
 
+    final theme = GptMarkdownTheme.of(context);
+    const dsfrTextStyle = DsfrTextStyle.headline3();
+    final conf = config.copyWith(
+      style: [dsfrTextStyle, dsfrTextStyle, theme.h3, theme.h4, theme.h5, theme.h6][match[1]!.length -
+          1]?.copyWith(color: config.style?.color),
+    );
+
     return Padding(
       padding: const EdgeInsets.only(bottom: DsfrSpacings.s1w),
       child: Row(
         spacing: DsfrSpacings.s1w,
         children: [
-          Icon(icon, size: 30, color: color),
-          Expanded(child: Text(namedGroup.replaceAll(RegExp(r'#+$'), ''), style: const DsfrTextStyle.headline3())),
+          Icon(icon, size: 30, color: iconColor),
+          Expanded(
+            child: Text.rich(
+              TextSpan(children: MarkdownComponent.generate(context, namedGroup.replaceAll(RegExp(r'#+$'), ''), conf, false)),
+            ),
+          ),
         ],
       ),
     );

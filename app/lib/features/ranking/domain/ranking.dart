88a91df22f00120681cf5df7d8382e73587c1912
Lucas SAUDON <lucas.saudon@beta.gouv.fr>
2025-06-17 09:34:24 +0200

    fix(classement): pourcentile peut être null et n'est pas utlisé

--- app/lib/features/ranking/domain/ranking.dart
+++ app/lib/features/ranking/domain/ranking.dart
@@ -3,24 +3,24 @@ import 'package:app/features/ranking/domain/user_rank.dart';
 import 'package:equatable/equatable.dart';
 
 class Ranking extends Equatable {
-  const Ranking({
-    required this.topThree,
-    required this.user,
-    required this.userRanking,
-    required this.percentile,
-    this.postalCode,
-    this.communeLabel,
-    required this.badges,
-  });
+  Ranking.fromJson(final Map<String, dynamic> json)
+    : topThree = (json['top_trois'] as List<dynamic>).map((final e) => UserRank.fromJson(e as Map<String, dynamic>)).toList(),
+      user = UserRank.fromJson(json['utilisateur'] as Map<String, dynamic>),
+      userRanking = (json['classement_utilisateur'] as List<dynamic>)
+          .map((final e) => UserRank.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      postalCode = json['code_postal'] as String?,
+      communeLabel = json['commune_label'] as String?,
+      badges = (json['badges'] as List<dynamic>).map((final e) => AchievementBadge.fromJson(e as Map<String, dynamic>)).toList();
 
   final List<UserRank> topThree;
   final UserRank user;
   final List<UserRank> userRanking;
-  final String percentile;
   final String? postalCode;
   final String? communeLabel;
+
   final List<AchievementBadge> badges;
 
   @override
-  List<Object?> get props => [topThree, user, userRanking, percentile, postalCode, communeLabel, badges];
+  List<Object?> get props => [topThree, user, userRanking, postalCode, communeLabel, badges];
 }

8946d828b61fe0a70fb34eb890a361c422d2bb3f
Lucas SAUDON <lucas.saudon@beta.gouv.fr>
2025-07-08 09:16:22 +0200

    chore: upgrade dcm and fix lints

--- app/test/features/bdd_hooks/hooks.dart
+++ app/test/features/bdd_hooks/hooks.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused-code, unused-files
+// ignore_for_file: unused-code, unused-files, avoid-unused-parameters
 import 'dart:async';
 
 import '../step/initialize_context.dart';

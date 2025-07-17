35d8a777d3af9eaf45498b6df456065e153f58f5
Lucas SAUDON <lucas.saudon@beta.gouv.fr>
2025-07-17 16:05:39 +0200

    refacto: améliorer l'affichage des svgs

--- app/lib/features/services/seasonal_fruits_and_vegetables/infrastructure/plant_mapper.dart
+++ app/lib/features/services/seasonal_fruits_and_vegetables/infrastructure/plant_mapper.dart
@@ -5,7 +5,7 @@ abstract final class PlantMapper {
     type: plantTypeFromJson(json['type_fruit_legume'] as String),
     title: json['titre'] as String,
     carbonFootprintInKg: (json['impact_carbone_kg'] as num).toDouble(),
-    imageUrl: json['image_url'] as String,
+    imageUrl: (json['image_url'] as String).split('/').last, // Prend uniquement le nom pour prendre en local
   );
 
   static PlantType plantTypeFromJson(final String json) => switch (json) {

0dbd3749d8f43f6215ba4c6d0289c37dd87ff22b
Lucas SAUDON <lucas.saudon@beta.gouv.fr>
2025-07-24 17:08:07 +0200

    refactor: supprimer l'utilisation de certains kDebugMode

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,9 +2,8 @@ import 'dart:async';
 
 import 'package:app/app/app_setup.dart';
 import 'package:app/core/error/infrastructure/error_handler.dart';
-import 'package:flutter/foundation.dart';
+import 'package:clock/clock.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
@@ -13,9 +12,5 @@ Future<void> main() async {
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await ErrorHandler.register();
 
-  if (defaultTargetPlatform == TargetPlatform.android) {
-    await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
-  }
-
-  runApp(const AppSetup());
+  runApp(const AppSetup(clock: Clock()));
 }

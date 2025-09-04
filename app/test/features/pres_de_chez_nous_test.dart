a593b87d21961339a1bc56e52a1d9fe74c576bd1
Lucas SAUDON <lucas.saudon@beta.gouv.fr>
2025-09-04 11:22:58 +0200

    fix(pdcn): téléphone liste ou seul

--- app/test/features/pres_de_chez_nous_test.dart
+++ app/test/features/pres_de_chez_nous_test.dart
@@ -12,7 +12,6 @@ import './step/the_application_is_launched.dart';
 import './step/i_tap_on_text.dart';
 import './step/i_tap_on.dart';
 import './step/i_see.dart';
-import './step/the_api_receive.dart';
 
 void main() {
   setUpAll(() async {
@@ -92,84 +91,10 @@ void main() {
                 "is_last": false,
                 "is_out_of_range": false
               }
-            ],
-            [
-              'GET',
-              '/utilisateurs/{userId}/recherche_services/proximite/categories',
-              200,
-              [
-                {
-                  "code": "nourriture",
-                  "label": "Tous les commerces",
-                  "is_default": true
-                },
-                {
-                  "code": "circuit_court",
-                  "label": "Les producteurs locaux",
-                  "is_default": false
-                },
-                {
-                  "code": "epicerie_superette",
-                  "label": "Les épiceries et supérettes",
-                  "is_default": false
-                },
-                {
-                  "code": "marche_local",
-                  "label": "Les marchés locaux",
-                  "is_default": false
-                },
-                {
-                  "code": "zero_dechet",
-                  "label": "Les boutiques zéro déchet",
-                  "is_default": false
-                }
-              ]
-            ],
-            [
-              'POST',
-              '/utilisateurs/{userId}/recherche_services/proximite/search2',
-              200,
-              {
-                "encore_plus_resultats_dispo": false,
-                "resultats": [
-                  {
-                    "id": "BG1",
-                    "titre": "EARL les maraîchers du Val d'Amour",
-                    "adresse_code_postal": "39120",
-                    "adresse_nom_ville": "Rahon",
-                    "adresse_rue": "6, ruelle des Merles",
-                    "est_favoris": false,
-                    "nombre_favoris": 0,
-                    "distance_metres": 605,
-                    "image_url": null,
-                    "description":
-                        "Maraîcher bio en demi gros. Légumes de saison.",
-                    "phone": "09 61 48 96 07",
-                    "categories": [
-                      "Alimentation et Agriculture",
-                      "Légumes",
-                      "Circuits courts",
-                      "Producteur / Artisan"
-                    ],
-                    "open_hours": [],
-                    "latitude": 46.99214,
-                    "longitude": 5.4661,
-                    "ingredients": [],
-                    "etapes_recette": [],
-                    "categories_labels": [
-                      "Alimentation et Agriculture",
-                      "Légumes",
-                      "Circuits courts",
-                      "Producteur / Artisan"
-                    ]
-                  }
-                ]
-              }
             ]
           ]));
       await theApplicationIsLaunched(tester);
       await iTapOnText(tester, 1, '🍛 Me nourrir');
-      await iTapOn(tester, 'Les commerces locaux');
     }
 
     Future<void> beforeEach(String title, [List<String>? tags]) async {
@@ -181,66 +106,251 @@ void main() {
       await Hooks.afterEach(title, success, tags);
     }
 
-    testWidgets('''Voir les suggestions''', (tester) async {
+    testWidgets('''Voir le détail avec un téléphone''', (tester) async {
       var success = true;
       try {
-        await beforeEach('''Voir les suggestions''');
+        await beforeEach('''Voir le détail avec un téléphone''');
         await bddSetUp(tester);
-        await iSee(tester, "EARL les maraîchers du Val d'Amour");
-        await theApiReceive(
+        await theApiWillReturn(
             tester,
             const bdd.DataTable([
-              ['method', 'path', 'statusCode', 'requestData'],
+              ['method', 'path', 'statusCode', 'responseData'],
+              [
+                'GET',
+                '/utilisateurs/{userId}/recherche_services/proximite/categories',
+                200,
+                [
+                  {
+                    "code": "nourriture",
+                    "label": "Tous les commerces",
+                    "is_default": true
+                  },
+                  {
+                    "code": "circuit_court",
+                    "label": "Les producteurs locaux",
+                    "is_default": false
+                  },
+                  {
+                    "code": "epicerie_superette",
+                    "label": "Les épiceries et supérettes",
+                    "is_default": false
+                  },
+                  {
+                    "code": "marche_local",
+                    "label": "Les marchés locaux",
+                    "is_default": false
+                  },
+                  {
+                    "code": "zero_dechet",
+                    "label": "Les boutiques zéro déchet",
+                    "is_default": false
+                  }
+                ]
+              ],
               [
                 'POST',
-                "/utilisateurs/{userId}/recherche_services/proximite/search2",
+                '/utilisateurs/{userId}/recherche_services/proximite/search2',
                 200,
                 {
-                  "categorie": "nourriture",
-                  "nombre_max_resultats": 9,
-                  "rayon_metres": 5000
+                  "encore_plus_resultats_dispo": true,
+                  "resultats": [
+                    {
+                      "id": "IAH",
+                      "titre": "Accorderie de Chambery",
+                      "site_web":
+                          "http://www.accorderie.fr/chambery/laccorderie-de-chambery/",
+                      "est_favoris": false,
+                      "nombre_favoris": 0,
+                      "distance_metres": 1506,
+                      "image_url":
+                          "https://presdecheznous.gogocarto.fr/uploads/presdecheznous/images/elements/CapOuPasCap/2018/11/logo.jpg",
+                      "description":
+                          "Le principe est simple, chacun fait pour les autres ce qu’il sait faire selon ses disponibilités. Quand un accordeur rend un service, il reçoit du bénéficiaire un chèque-temps qu’il pourra utiliser plus tard pour demander à d’autres un coup de main.\\r\nLes services échangés sont comptabilisés grâce à des chèques-temps sur la base du temps réel nécessaire pour rendre le service demandé. La comptabilité se fait à partir d’un compte-temps où sont relevés les heures données et reçues.\\r\nLors de son inscription, tout nouveau accordeur reçoit sur son compte temps un crédit de 15h00 pour lui permettre d’échanger des services immédiatement.\\r\nA Chambéry, les 1411 Accordeur(e)s ont déjà échangé plus de 12600 heures de services.",
+                      "phone": "04 79 96 13 49",
+                      "categories": ["S.E.L / R.E.R.S", "Systèmes d'échanges"],
+                      "openhours_more_infos":
+                          "mardi 16-20H, mercredi 14h - 17h, jeudi 10h-16h",
+                      "open_hours": [],
+                      "latitude": 45.59573,
+                      "longitude": 5.91668,
+                      "ingredients": [],
+                      "etapes_recette": [],
+                      "categories_labels": [
+                        "S.E.L / R.E.R.S",
+                        "Systèmes d'échanges"
+                      ],
+                      "adresse_complete": ""
+                    }
+                  ]
+                }
+              ],
+              [
+                'GET',
+                '/utilisateurs/{userId}/recherche_services/proximite/last_results/IAH',
+                200,
+                {
+                  "id": "IAH",
+                  "titre": "Accorderie de Chambery",
+                  "site_web":
+                      "http://www.accorderie.fr/chambery/laccorderie-de-chambery/",
+                  "est_favoris": false,
+                  "nombre_favoris": 0,
+                  "distance_metres": 1506,
+                  "image_url":
+                      "https://presdecheznous.gogocarto.fr/uploads/presdecheznous/images/elements/CapOuPasCap/2018/11/logo.jpg",
+                  "description":
+                      "Le principe est simple, chacun fait pour les autres ce qu’il sait faire selon ses disponibilités. Quand un accordeur rend un service, il reçoit du bénéficiaire un chèque-temps qu’il pourra utiliser plus tard pour demander à d’autres un coup de main.\\r\nLes services échangés sont comptabilisés grâce à des chèques-temps sur la base du temps réel nécessaire pour rendre le service demandé. La comptabilité se fait à partir d’un compte-temps où sont relevés les heures données et reçues.\\r\nLors de son inscription, tout nouveau accordeur reçoit sur son compte temps un crédit de 15h00 pour lui permettre d’échanger des services immédiatement.\\r\nA Chambéry, les 1411 Accordeur(e)s ont déjà échangé plus de 12600 heures de services.",
+                  "phone": "04 79 96 13 49",
+                  "categories": ["S.E.L / R.E.R.S", "Systèmes d'échanges"],
+                  "openhours_more_infos":
+                      "mardi 16-20H, mercredi 14h - 17h, jeudi 10h-16h",
+                  "open_hours": [],
+                  "latitude": 45.59573,
+                  "longitude": 5.91668,
+                  "ingredients": [],
+                  "etapes_recette": [],
+                  "categories_labels": [
+                    "S.E.L / R.E.R.S",
+                    "Systèmes d'échanges"
+                  ],
+                  "adresse_complete": ""
                 }
               ]
             ]));
+        await iTapOn(tester, 'Les commerces locaux');
+        await iTapOn(tester, 'Accorderie de Chambery');
+        await iSee(tester, 'Accorderie de Chambery');
       } catch (_) {
         success = false;
         rethrow;
       } finally {
         await afterEach(
-          '''Voir les suggestions''',
+          '''Voir le détail avec un téléphone''',
           success,
         );
       }
     });
-    testWidgets('''Choisir une catégorie''', (tester) async {
+    testWidgets('''Voir le détail avec une liste de téléphone''',
+        (tester) async {
       var success = true;
       try {
-        await beforeEach('''Choisir une catégorie''');
+        await beforeEach('''Voir le détail avec une liste de téléphone''');
         await bddSetUp(tester);
-        await iTapOn(tester, 'Tous les commerces');
-        await iTapOn(tester, 'Les producteurs locaux');
-        await iSee(tester, "EARL les maraîchers du Val d'Amour");
-        await theApiReceive(
+        await theApiWillReturn(
             tester,
             const bdd.DataTable([
-              ['method', 'path', 'statusCode', 'requestData'],
+              ['method', 'path', 'statusCode', 'responseData'],
+              [
+                'GET',
+                '/utilisateurs/{userId}/recherche_services/proximite/categories',
+                200,
+                [
+                  {
+                    "code": "nourriture",
+                    "label": "Tous les commerces",
+                    "is_default": true
+                  },
+                  {
+                    "code": "circuit_court",
+                    "label": "Les producteurs locaux",
+                    "is_default": false
+                  },
+                  {
+                    "code": "epicerie_superette",
+                    "label": "Les épiceries et supérettes",
+                    "is_default": false
+                  },
+                  {
+                    "code": "marche_local",
+                    "label": "Les marchés locaux",
+                    "is_default": false
+                  },
+                  {
+                    "code": "zero_dechet",
+                    "label": "Les boutiques zéro déchet",
+                    "is_default": false
+                  }
+                ]
+              ],
               [
                 'POST',
-                "/utilisateurs/{userId}/recherche_services/proximite/search2",
+                '/utilisateurs/{userId}/recherche_services/proximite/search2',
                 200,
                 {
-                  "categorie": "circuit_court",
-                  "nombre_max_resultats": 9,
-                  "rayon_metres": 5000
+                  "encore_plus_resultats_dispo": true,
+                  "resultats": [
+                    {
+                      "id": "IAB",
+                      "titre": "Accorderie de Chambery",
+                      "site_web":
+                          "http://www.accorderie.fr/chambery/laccorderie-de-chambery/",
+                      "est_favoris": false,
+                      "nombre_favoris": 0,
+                      "distance_metres": 1506,
+                      "image_url":
+                          "https://presdecheznous.gogocarto.fr/uploads/presdecheznous/images/elements/CapOuPasCap/2018/11/logo.jpg",
+                      "description":
+                          "Le principe est simple, chacun fait pour les autres ce qu’il sait faire selon ses disponibilités. Quand un accordeur rend un service, il reçoit du bénéficiaire un chèque-temps qu’il pourra utiliser plus tard pour demander à d’autres un coup de main.\\r\nLes services échangés sont comptabilisés grâce à des chèques-temps sur la base du temps réel nécessaire pour rendre le service demandé. La comptabilité se fait à partir d’un compte-temps où sont relevés les heures données et reçues.\\r\nLors de son inscription, tout nouveau accordeur reçoit sur son compte temps un crédit de 15h00 pour lui permettre d’échanger des services immédiatement.\\r\nA Chambéry, les 1411 Accordeur(e)s ont déjà échangé plus de 12600 heures de services.",
+                      "phone": ["04 79 96 13 49"],
+                      "categories": ["S.E.L / R.E.R.S", "Systèmes d'échanges"],
+                      "openhours_more_infos":
+                          "mardi 16-20H, mercredi 14h - 17h, jeudi 10h-16h",
+                      "open_hours": [],
+                      "latitude": 45.59573,
+                      "longitude": 5.91668,
+                      "ingredients": [],
+                      "etapes_recette": [],
+                      "categories_labels": [
+                        "S.E.L / R.E.R.S",
+                        "Systèmes d'échanges"
+                      ],
+                      "adresse_complete": ""
+                    }
+                  ]
+                }
+              ],
+              [
+                'GET',
+                '/utilisateurs/{userId}/recherche_services/proximite/last_results/IAB',
+                200,
+                {
+                  "id": "IAB",
+                  "titre": "Accorderie de Chambery",
+                  "site_web":
+                      "http://www.accorderie.fr/chambery/laccorderie-de-chambery/",
+                  "est_favoris": false,
+                  "nombre_favoris": 0,
+                  "distance_metres": 1506,
+                  "image_url":
+                      "https://presdecheznous.gogocarto.fr/uploads/presdecheznous/images/elements/CapOuPasCap/2018/11/logo.jpg",
+                  "description":
+                      "Le principe est simple, chacun fait pour les autres ce qu’il sait faire selon ses disponibilités. Quand un accordeur rend un service, il reçoit du bénéficiaire un chèque-temps qu’il pourra utiliser plus tard pour demander à d’autres un coup de main.\\r\nLes services échangés sont comptabilisés grâce à des chèques-temps sur la base du temps réel nécessaire pour rendre le service demandé. La comptabilité se fait à partir d’un compte-temps où sont relevés les heures données et reçues.\\r\nLors de son inscription, tout nouveau accordeur reçoit sur son compte temps un crédit de 15h00 pour lui permettre d’échanger des services immédiatement.\\r\nA Chambéry, les 1411 Accordeur(e)s ont déjà échangé plus de 12600 heures de services.",
+                  "phone": ["04 79 96 13 49", "04 79 96 13 49"],
+                  "categories": ["S.E.L / R.E.R.S", "Systèmes d'échanges"],
+                  "openhours_more_infos":
+                      "mardi 16-20H, mercredi 14h - 17h, jeudi 10h-16h",
+                  "open_hours": [],
+                  "latitude": 45.59573,
+                  "longitude": 5.91668,
+                  "ingredients": [],
+                  "etapes_recette": [],
+                  "categories_labels": [
+                    "S.E.L / R.E.R.S",
+                    "Systèmes d'échanges"
+                  ],
+                  "adresse_complete": ""
                 }
               ]
             ]));
+        await iTapOn(tester, 'Les commerces locaux');
+        await iTapOn(tester, 'Accorderie de Chambery');
+        await iSee(tester, 'Accorderie de Chambery');
       } catch (_) {
         success = false;
         rethrow;
       } finally {
         await afterEach(
-          '''Choisir une catégorie''',
+          '''Voir le détail avec une liste de téléphone''',
           success,
         );
       }
